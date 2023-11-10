56c4b5d644fa71c4705d637b9ece64fc75b12dea
Tristan Barnes <tristan.barnes@colorado.edu>
2023-11-09 17:42:01 -0700

    last commits of Tristan branch

--- All_Project_Code_And_Components/init_data/create.sql
+++ All_Project_Code_And_Components/init_data/create.sql
@@ -1,27 +1,23 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE IF NOT EXISTS users (
   user_id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(100),
-  username VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL
+  username SERIAL VARCHAR(100) NOT NULL,
+  password char(60) NOT NULL
 );
 
 DROP TABLE IF EXISTS books CASCADE;
 CREATE TABLE IF NOT EXISTS books (
   book_id SERIAL PRIMARY KEY NOT NULL,
   book_name VARCHAR(100) NOT NULL,
-  author VARCHAR(100),
-  genre VARCHAR(100) CONSTRAINT limited_values CHECK (genre in ('fiction', 'non-fiction', 'sci-fi', 'novel', 'mystery', 'thriller'))
+  author VARCHAR(100)
 );
 
 DROP TABLE IF EXISTS reviews CASCADE;
 CREATE TABLE IF NOT EXISTS reviews (
   review_id SERIAL PRIMARY KEY NOT NULL,
-  book_id SERIAL NOT NULL,
   username VARCHAR(100),
   review VARCHAR(200),
-  rating DECIMAL NOT NULL,
-  FOREIGN KEY (book_id) REFERENCES books (book_id)
+  rating DECIMAL NOT NULL
 );
 
 DROP TABLE IF EXISTS books_to_users CASCADE;
@@ -38,4 +34,12 @@ CREATE TABLE IF NOT EXISTS books_to_reviews (
   review_id SERIAL NOT NULL,
   FOREIGN KEY (book_id) REFERENCES books (book_id),
   FOREIGN KEY (review_id) REFERENCES reviews (review_id)
-)
+);
+
+DROP TABLE IF EXISTS reviews_to_users CASCADE;
+CREATE TABLE IF NOT EXISTS reviews_to_users (
+    user_id SERIAL NOT NUll,
+    review_id SERITAL NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (review_id) REFERENCES reviews (review_id)
+);
