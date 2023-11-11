509a82674b48a93aca8b26de12d3742ffced7056
suman <suman@sumans-MBP.hsd1.co.comcast.net>
2023-11-11 12:11:28 -0700

    made changes to section 4, added routing for add_reviews , registraion, login but the authuntication has not been finished

--- All_Project_Code_And_Components/index.js
+++ All_Project_Code_And_Components/index.js
@@ -62,9 +62,107 @@ app.use(
 // TODO - Include your API routes here
 
 app.get("/", (req,res) => {
-  res.render('pages/home');
+  res.render('pages/login');
 });
 
+
+// Redirect from root to /login
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+  });
+
+app.post('/login', async (req, res) => {
+    try {
+      const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', req.body.username);
+      if (user && await bcrypt.compare(req.body.password, user.password)) {
+        req.session.user = user;
+        res.redirect('/welcome');
+      } else {
+        throw new Error("Incorrect username or password.");
+      }
+    } catch (error) {
+      console.error("Login Error:", error);
+      res.render('pages/welcome', { message: "Incorrect username or password." });
+    }
+  });
+
+  
+
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+  });
+// Register
+app.post('/register', async (req, res) => {
+  try {
+      console.log('Attempting to register user:', req.body.username); 
+      const hash = await bcrypt.hash(req.body.password, 10);
+      await db.none('INSERT INTO users (username, password) VALUES ($1, $2)', [req.body.username, hash]);
+      console.log('User registered successfully:', req.body.username); 
+      res.redirect('/login');
+  } catch (error) {
+      console.error("Registration Error:", error);
+      res.render('pages/login', { message: "Registration failed. Please try again." });
+  }
+});
+app.get('/welcome', (req, res) => {
+  res.render('pages/welcome');
+});
+app.get('/home', (req, res) => {
+  res.render('pages/welcome');
+});
+
+const auth = (req, res, next) => {
+  if (req.session.user) {
+    return res.redirect('/login');
+  }
+  next();
+};
+app.use(auth);
+  
+// Route to display book reviews
+app.get('/reviews', auth, async (req, res) => {
+  try {
+    const reviews = await db.any('SELECT * FROM reviews'); // Assuming 'reviews' is your table name
+    res.render('pages/reviews', { reviews });
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    res.render('pages/error', { message: "Error fetching reviews." });
+  }
+});
+app.get('/add_reviews', auth, (req, res) => {
+  console.log('Rendering add_review page');
+  res.render('pages/add_reviews');
+});
+
+// Route to add a new book review
+app.post('/add_reviews', auth, async (req, res) => {
+  try {
+    const { title, author, review } = req.body;
+    await db.none('INSERT INTO reviews (title, author, review) VALUES ($1, $2, $3)', [title, author, review]);
+    res.redirect('/pages/add_reviews');
+  } catch (error) {
+    console.error("Error adding review:", error);
+    res.render('pages/error', { message: "Error adding review." });
+  }
+});
+
+
+
+
+  app.get('/logout', (req, res) => {
+    // Destroy the session
+    req.session.destroy((err) => {
+      if (err) {
+        console.error("Error in session destruction:", err);
+      }
+      res.render('pages/login', { message: "Logged out Successfully" });
+    });
+  });
+  
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
